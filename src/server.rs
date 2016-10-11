8665af9bbce99ba1c4fbc0c05666feeb22d2387c
Gexon <alimgan@gmail.com>
2016-10-12 02:40:25 +0500

    пилим чат-сервер из mio

--- src/server.rs
+++ src/server.rs
@@ -41,6 +41,8 @@ impl Server {
         }
     }
 
+    /// Старт сервака собственно.
+    /// тут же бесконечный цикл обработки событйи.
     pub fn run(&mut self, poll: &mut Poll) -> io::Result<()> {
         try!(self.register(poll));
 
@@ -71,4 +73,85 @@ impl Server {
             self.tick(poll);
         }
     }
+
+    /// Регистрация серверного опросника событий.
+    ///
+    /// This keeps the registration details neatly tucked away inside of our implementation.
+    /// Это хранит регистрационные данные аккуратно спрятан внутри нашей реализации.
+    pub fn register(&mut self, poll: &mut Poll) -> io::Result<()> {
+        poll.register(
+            &self.sock,
+            self.token,
+            Ready::readable(),
+            PollOpt::edge()
+        ).or_else(|e| {
+            error!("Ошибка регистрации опросника событий {:?}, {:?}", self.token, e);
+            Err(e)
+        })
+    }
+
+    /// обработчик события
+    fn ready(&mut self, poll: &mut Poll, token: Token, event: Ready) {
+        debug!("токен {:?} событие = {:?}", token, event);
+
+        if event.is_error() {
+            warn!("Ошибка события токена{:?}", token);
+            self.find_connection_by_token(token).mark_reset(); // пометить на сброс соединения
+            return;
+        }
+
+        if event.is_hup() {
+            trace!("Событие обрыва соединения(Hup event for) токена {:?}", token);
+            self.find_connection_by_token(token).mark_reset();
+            return;
+        }
+
+        // Мы не обнаружили ошибок записи событий для токена нашего сервера.
+        // Запись события для прочих токенов, должны передаваться этому подключению.
+        if event.is_writable() {
+            trace!("Записываем событие для токена {:?}", token);
+            assert!(self.token != token, "Получение записанного события для Сервера");
+
+            let conn = self.find_connection_by_token(token);
+
+            if conn.is_reset() {
+                info!("{:?} соединение сброшено", token);
+                return;
+            }
+
+            conn.writable()
+                .unwrap_or_else(|e| {
+                    warn!("Ошибка записи события для токена {:?}, {:?}", token, e);
+                    conn.mark_reset();
+                });
+        }
+
+        // A read event for our `Server` token means we are establishing a new connection.
+        // Событие чтения для токена нашего сервера означает, что мы устанавливаем новое соединение.
+        // A read event for any other token should be handed off to that connection.
+        // Событие чтения для любого другого токена должны быть передан этому соединению.
+        if event.is_readable() {
+            trace!("Читаем событие для токена {:?}", token);
+            if self.token == token {
+                self.accept(poll);
+            } else {
+
+                if self.find_connection_by_token(token).is_reset() {
+                    info!("{:?} соединение сброшено", token);
+                    return;
+                }
+
+                self.readable(token)
+                    .unwrap_or_else(|e| {
+                        warn!("Ошибка чтения события для токена {:?}: {:?}", token, e);
+                        self.find_connection_by_token(token).mark_reset();
+                    });
+            }
+        }
+
+        if self.token != token {
+            self.find_connection_by_token(token).mark_idle();
+        }
+    }
+
 }
